48912871ae2b2f03c1e24c69f566ce40f72731d4
Navid <navidtaheri@gmail.com>
2024-12-15 09:47:57 +0100

    Delete port_finder_backend directory
    port_finder_backed removed!

--- port_finder_backend/src/models/port.model.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const portSchema = new mongoose.Schema({
-  original: { type: String, required: true },
-  normalized: { type: String, required: true }, // e.g., for deduplicated or cleaned data
-});
-
-export const Port = mongoose.model("Port", portSchema);
